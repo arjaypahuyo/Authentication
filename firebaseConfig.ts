e4333dbf47b3d9f7ca025dc415672c93d48d9950
arjaypahuyo <20211359@nbsc.edu.ph>
2024-05-09 17:41:11 +0800

    Final

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,12 +4,12 @@ import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCcTb2izQeXizmerTsSW90STYM0zoBrQPY",
-  authDomain: "authentication-83d8b.firebaseapp.com",
-  projectId: "authentication-83d8b",
-  storageBucket: "authentication-83d8b.appspot.com",
-  messagingSenderId: "244482784286",
-  appId: "1:244482784286:web:ff619344af77e0eafe420f"
+  apiKey: "AIzaSyBiqnFjjVwg2xv7oG4hP61N7ZQmgwEfZuY",
+  authDomain: "todolist-6b73d.firebaseapp.com",
+  projectId: "todolist-6b73d",
+  storageBucket: "todolist-6b73d.appspot.com",
+  messagingSenderId: "1019153212067",
+  appId: "1:1019153212067:web:f5d8851e75d84d87389516"
 };
 
 // Initialize Firebase
